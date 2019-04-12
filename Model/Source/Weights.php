568d91471cb3166f2a1f6e6eadc89033fc0bf31f
Marcio Junior <marciofmjr@hotmail.com>
2019-04-12 10:35:49 -0300

    Format Weights class using phpcs

--- Model/Source/Weights.php
+++ Model/Source/Weights.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * Weights.php
+ * 
+ * @category Cammino
+ * @package  Cammino_Shipping
+ * @author   Cammino Digital <suporte@cammino.com.br>
+ * @license  http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link     https://github.com/cammino/magento-shipping
+ */
 class  Cammino_Shipping_Model_Source_Weights
 {
+    /**
+     * Function responsible for returning weights labels
+     * 
+     * @return array
+     */
     public function toOptionArray()
     {
         $weights = array(
